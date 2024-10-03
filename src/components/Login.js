05ecf62d5e767d7160eac21a6eebd881152da9f4
DarshilGajera07 <darshilgajera07@gmail.com>
2024-10-03 14:10:13 +0530

    .

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("https://inotebook-umber-omega.vercel.app/login/api/auth/login", {
+        const response = await fetch("https://inotebook-umber-omega.vercel.app/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
--- src/components/Singup.js
+++ src/components/Singup.js
@@ -8,7 +8,7 @@ const Singup = (props) => {
   const handleSubmit = async (e) => {
     const { name, email, password } = credentials
     e.preventDefault();
-    const response = await fetch("https://inotebook-umber-omega.vercel.app/signup/api/auth/createuser", {
+    const response = await fetch("https://inotebook-umber-omega.vercel.app/api/auth/createuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
